0357c96aaf66e7a84f181b9675f3fa160d71daf2
Kalyan Akella <kalyan.akella@gmail.com>
2011-12-25 08:24:14 +0530

    WordCount impl. using in-mapper combining

---
+++ wordcount/WordCount.java
@@ -0,0 +1,103 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class WordCount extends Configured implements Tool {
+
+    public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+        private HashMap<String,Integer> buffer;
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            buffer = new HashMap<String, Integer>();
+        }
+
+        // Uses in-mapper combining !
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer tokenizer = new StringTokenizer(value.toString());
+            while (tokenizer.hasMoreTokens()) {
+                String word = tokenizer.nextToken();
+                if (buffer.containsKey(word)) {
+                    buffer.put(word, buffer.get(word) + 1);
+                } else {
+                    buffer.put(word, 1);
+                }
+            }
+        }
+
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            for (String key : buffer.keySet()) {
+                context.write(new Text(key), new IntWritable(buffer.get(key)));
+            }
+        }
+    }
+
+    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            super.setup(context);
+        }
+
+        @Override
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable value : values) {
+                sum += value.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            super.cleanup(context);
+        }
+    }
+
+    public int run(String[] args) throws Exception {
+        Job job = new Job();
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setMapperClass(Map.class);
+        job.setCombinerClass(Reduce.class);
+        job.setReducerClass(Reduce.class);
+        
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setJarByClass(WordCount.class);
+
+//        job.submit();
+        job.waitForCompletion(true);
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration configuration = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(configuration, args).getRemainingArgs();
+        ToolRunner.run(new WordCount(), otherArgs);
+    }
+}
